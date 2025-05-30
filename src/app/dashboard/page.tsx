e1f88457871d83d5a393ad6d3cd5e4fe50593be4
Ausath Ikram <ausathdzil@gmail.com>
2025-05-30 14:00:40 +0700

    Add sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,14 @@
-import { SignOutButton } from '@/components/sign-out-button';
-import { auth } from '@/lib/auth';
-import { headers } from 'next/headers';
+import { db } from '@/db';
+import { analysisLog } from '@/db/schema';
+
+async function getAnalysisLogs() {
+  const data = await db.select().from(analysisLog);
+
+  return data;
+}
 
 export default async function Dashboard() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+  const analysisLogs = await getAnalysisLogs();
 
-  return (
-    <div>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-      <SignOutButton />
-    </div>
-  );
+  return <pre>{JSON.stringify(analysisLogs, null, 2)}</pre>;
 }
